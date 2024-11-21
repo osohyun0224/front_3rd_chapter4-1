3d9cbb6dbbe146af4c8720133e0ce2136a1ebb94
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-22 03:09:41 +0900

    ♻️ refactor :: 기존의 ttf -> woff 로 파일 형식 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,46 +5,41 @@ import "./globals.css";
 const pretendard = localFont({
   src: [
     {
-      path: "./fonts/Pretendard-Black.ttf",
+      path: "./fonts/Pretendard-Black.woff",
       weight: "900",
     },
     {
-      path: "./fonts/Pretendard-ExtraBold.ttf",
+      path: "./fonts/Pretendard-ExtraBold.woff",
       weight: "800",
     },
     {
-      path: "./fonts/Pretendard-Bold.ttf",
+      path: "./fonts/Pretendard-Bold.woff",
       weight: "700",
     },
     {
-      path: "./fonts/Pretendard-SemiBold.ttf",
+      path: "./fonts/Pretendard-SemiBold.woff",
       weight: "600",
     },
     {
-      path: "./fonts/Pretendard-Medium.ttf",
-      weight: "800",
-    },
-    {
-      path: "./fonts/Pretendard-Bold.ttf",
-      weight: "700",
-    },
-    {
-      path: "./fonts/Pretendard-Medium.ttf",
+      path: "./fonts/Pretendard-Medium.woff",
       weight: "500",
     },
     {
-      path: "./fonts/Pretendard-Regular.ttf",
+      path: "./fonts/Pretendard-Regular.woff",
       weight: "400",
     },
     {
-      path: "./fonts/Pretendard-Light.ttf",
+      path: "./fonts/Pretendard-Light.woff",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Pretendard-ExtraLight.woff",
       weight: "200",
     },
     {
-      path: "./fonts/Pretendard-Thin.ttf",
+      path: "./fonts/Pretendard-Thin.woff",
       weight: "100",
     },
-
   ],
   variable: "--font-pretendard",
 });
