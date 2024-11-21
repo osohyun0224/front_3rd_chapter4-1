2493c5f90a9ed7a4e4ffd0c3188740eda6223d70
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-22 02:58:12 +0900

    🔥 remove :: css 분리가 필요없는 파일로 다시 롤백

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -10,22 +7,6 @@ const nextConfig = {
         ...config.optimization,
         minimize: true,
       };
-
-      config.plugins.push(
-        new MiniCssExtractPlugin({
-          filename: 'static/css/[name].[contenthash].css',
-          chunkFilename: 'static/css/[id].[contenthash].css',
-        })
-      );
-
-      config.optimization.minimizer.push(
-        new CssMinimizerPlugin()
-      );
-
-      config.module.rules.push({
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      });
     }
     return config;
   },
