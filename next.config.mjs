c16b8b323568a7e9651cc4b6ebd432043ee8922b
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-11-22 02:40:34 +0900

    ✨ feat :: 코드를 minify하기 위해 next.config.mjs 파일에 Webpack의 optimization 설정을 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
